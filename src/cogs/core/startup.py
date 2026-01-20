914143531d102fa21700921ff059d374f75f1c7b
xNiloGamerx <nikb0322@gmail.com>
2026-01-20 20:17:41 +0100

    Initial commit

---
+++ src/cogs/core/startup.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+class Startup(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Logged in as {self.bot.user} (ID: {self.bot.user.id})')
+        print('------')
+
+
+async def setup(bot):
+    await bot.add_cog(Startup(bot))
---
+++ src/main.py
@@ -0,0 +1,57 @@
+import discord
+from discord.ext import commands
+
+import logging
+from dotenv import load_dotenv
+import os
+
+import signal
+import sys
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+handler = logging.FileHandler(filename=r'C:\Users\nikb0\Documents\01_Programming\01_Projects\DarkBot\src\log\discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+extensions = [
+    "cogs.core.startup",
+]
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send(f'Hello {message.author.name}!')
+
+    await bot.process_commands(message)
+
+@bot.event
+async def setup_hook():
+    for ext in extensions:
+        await bot.load_extension(ext)
+
+def shutdown_handler(sig, frame):
+    print("Bot wird beendet...")
+    
+    print(f"Database connection can be closed here: {__file__}")
+    # Database().db.close()
+
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, shutdown_handler)   # Ctrl+C
+signal.signal(signal.SIGTERM, shutdown_handler)  # kill
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
